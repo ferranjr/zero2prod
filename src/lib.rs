00c41f6a2b3dee91c25a974af6e816c3beb6ec1f
Ferran Puig Calvache <ferran.puigcalvache@sky.uk>
2024-01-20 12:01:35 +0000

    3.8 set up postgres and adding test

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,16 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 use std::net::TcpListener;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
+            .route("/health_check", web::get().to(routes::health_check))
+            .route("/subscriptions", web::post().to(routes::subscribe))
     })
     .listen(listener)?
     .run();
