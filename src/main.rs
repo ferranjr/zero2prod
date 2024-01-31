ff5600e08606badbf0937562ebbb26dbd7f1e40f
Ferran Puig Calvache <ferran.puigcalvache@sky.uk>
2024-01-31 23:45:16 +0000

    Chapter 7.3 Refactor integration tests for better modularity

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::{startup, telemetry};
+use zero2prod::startup::Application;
+use zero2prod::telemetry;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -10,24 +8,9 @@ async fn main() -> Result<(), std::io::Error> {
     telemetry::init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
 
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
 
-    startup::run(listener, connection_pool, email_client)?.await
+    Ok(())
 }
